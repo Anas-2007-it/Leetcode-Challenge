493a9c0f4e55123f1bd85b95c50ebc7c6755bc4e
Anas-2007-it <mohammedanas.muslim@gmail.com>
2026-08-25 18:01:56 +0530

    Time: 2 ms (68.2%), Space: 45.1 MB (66.75%) - LeetHub

---
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+class Solution {
+    public int missingMultiple(int[] nums, int k) {
+        HashSet<Integer> set=new HashSet<>();
+        for(int x:nums){
+          set.add(x);
+        }
+        int multiple=k;
+        while(set.contains(multiple)){
+          multiple+=k;
+        }
+        return multiple;
+
+
+          
+        
+    }
+}
