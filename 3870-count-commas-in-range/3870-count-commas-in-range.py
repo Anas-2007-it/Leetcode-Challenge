02731767bd4e1aa24d0493269faba82801fc1158
Anas-2007-it <mohammedanas.muslim@gmail.com>
2026-09-08 19:52:31 +0530

    Time: 0 ms (100%), Space: 12.3 MB (65.05%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.py
+++ 3870-count-commas-in-range/3870-count-commas-in-range.py
@@ -4,11 +4,6 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        count=0
-        for i in range(1,n+1):
-            digit=len(str(i))
-            if digit>=4:
-                count+=(digit-1)//3 # Imp formula ---> d=4  d-1//3=1 (1000)--> 1 comma
-        return count
-              
+        # OPTIMISED 
+        return max(0,n-999)
         
