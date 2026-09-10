a3ca797acf5f72b909f5889936b845826a82b91b
Anas-2007-it <mohammedanas.muslim@gmail.com>
2026-09-10 20:53:43 +0530

    Time: 0 ms (100%), Space: 43.1 MB (12.15%) - LeetHub

---
+++ 0917-reverse-only-letters/0917-reverse-only-letters.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String reverseOnlyLetters(String s) {
+        char res[]=s.toCharArray();
+        int left=0;
+        int right=s.length()-1;
+        while(left<right){
+            if(!Character.isLetter(res[left])){
+                left++;
+            }
+            else if(!Character.isLetter(res[right])){
+                right--;
+            }
+            else{
+            char temp=res[left];
+            res[left]=res[right];
+            res[right]=temp;
+            left++;
+            right--;
+            }
+        }
+        return new String(res);
+        
+    }
+}
